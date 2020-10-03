bfd353ca0326a5161cf97674376c44af3a2ee285
itsmeow <mods.itsmeow@gmail.com>
2020-10-03 17:14:32 -0400

    Add PR info to progress endpoint

--- src/api/users.js
+++ src/api/users.js
@@ -5,16 +5,33 @@ const getEventStage = require("../lib/stage");
 
 router.get("/:id/progress", async function (req, res, next) {
   const { id } = req.params;
-
   try {
     const contributions = await knex
-      .table("contribution")
-      .select("participant_id", "repo_id")
-      .groupByRaw("participant_id, repo_id")
-      .where({ participant_id: id });
-
-    return res.json({ unique: contributions.length });
+      .select("*")
+      .from((k) =>
+        k
+          .table("contribution")
+          .select(
+            "contribution.repo_id",
+            "contribution.pr_id",
+            "contribution.valid",
+            "pullrequest.url",
+            "pullrequest.created_at"
+          )
+          .select("sponsor.name as sponsor_name")
+          .select("repository.name as repository_name")
+          .where({ participant_id: id, valid: true })
+          .leftJoin("pullrequest", "contribution.pr_id", "pullrequest.id")
+          .leftJoin("repository", "contribution.repo_id", "repository.id")
+          .leftJoin("sponsor", "repository.sponsor_id", "sponsor.id")
+          .orderBy("pullrequest.created_at", "contribution.pr_id")
+          .as("t")
+      )
+      .distinctOn("repo_id");
+    console.log(contributions);
+    return res.json({ unique: contributions.length, prs: contributions });
   } catch (error) {
+    console.log(error);
     res.status(400);
     return res.json({ error: "Unable to get progress for user" });
   }
