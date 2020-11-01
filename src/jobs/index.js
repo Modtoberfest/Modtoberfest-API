bd01264b0f5a6bf702a579d893a8cdae359d6abd
Antoine <antoine.gagnon760@gmail.com>
2020-10-31 21:17:53 -0400

    Don't schedule task after event

--- src/jobs/index.js
+++ src/jobs/index.js
@@ -1,16 +1,19 @@
 const e = require("express");
 
 const trackerJob = require("./tracker").job;
+const getEventStage = require("../lib/stage"); 
 
 function schedule() {
   console.log("[Task Manager] Scheduling tasks...");
 
   if (process.env.DISABLE_TASK) {
     console.log("[Task Manager] Tasks are disabled, skipping.");
-    // require("./tracker").run();
+    require("./tracker").run();
   } else {
-    trackerJob.start();
-    console.log("[Task Manager] Tasks scheduled.");
+    if(getEventStage() === "mid") {
+      trackerJob.start();
+      console.log("[Task Manager] Tasks scheduled.");
+    }
   }
 }
 
